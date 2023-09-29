8db4c89e0e8166ef1b9598a31a09e69f97dfdab6
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2023-09-29 15:44:08 -0300

    Update E1-01.c

--- logic.c/problems.2/E1-01.c
+++ logic.c/problems.2/E1-01.c
@@ -1,1 +1,21 @@
+/*1. Escreva um algoritmo que calcule a média de quatro números introduzidos pelo usuário.
+*/
+#include<stdio.h>
+void main(){
+float n1, n2, n3, n4;
+float soma;
+float media;
 
+printf("Digite o primeiro numero: ");
+scanf("%f", &n1);
+printf("Digite o segundo numero: ");
+scanf("%f", &n2);
+printf("Digite o terceiro numero: ");
+scanf("%f", &n3);
+printf("Digite o quarto numero: ");
+scanf("%f", &n4);
+
+soma = n1 + n2 + n3 + n4;
+media = soma / 4;
+printf("A media eh: %f", media);
+}
