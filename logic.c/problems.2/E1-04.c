e096a23d2e407c5b4edabc8e2bb357f94d0fcdcf
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2023-09-29 15:50:24 -0300

    Create E1-04.c

---
+++ logic.c/problems.2/E1-04.c
@@ -0,0 +1,17 @@
+/* 4. Escreva um algoritmo que receba o valor do salário mínimo e o valor do salário de um
+funcionário, calcule e mostre a quantidade de salários mínimos que esse funcionário
+ganha.
+ */
+ #include<stdio.h>
+ void main(){
+ float salario,salariomin,quantidadesalario;
+
+ printf("digite o valor do salario minimo: ");
+ scanf("%f",&salariomin);
+
+ printf("digite o seu salario: ");
+ scanf("%f",&salario);
+
+ quantidadesalario = salario/salariomin;
+ printf("a quantidade de salarios minimos recebidos : %f",quantidadesalario);
+ }
