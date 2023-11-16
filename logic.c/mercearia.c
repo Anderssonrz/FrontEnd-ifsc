3d0d8df16e4684fac774ceeddee76587943b51e3
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2023-11-16 17:27:49 -0300

    Update mercearia.c

--- logic.c/mercearia.c
+++ logic.c/mercearia.c
@@ -5,32 +5,34 @@ O algoritmo deve atualizar o estoque e apresentar uma nota fiscal com o valor to
 Alem disso, o algoritmo deve permitir que o usuario aumente o estoque dos produtos, informando o nome do produto e a quantidade a ser adicionada.
 Ao finalizar a operacao, o algoritmo deve perguntar ao usuario se deseja encerrar o programa..*/
 
-#include < stdio.h >
-#include < stdlib.h >
-#include < locale.h >
-#include < math.h >
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+#define TAM 1000
 
+char nome[TAM];
+int qntde[TAM], cont = 0;
+float valor[TAM];
 
 int main()
 {
    setlocale(LC_ALL, "Portuguese");
    int option;
-
-   printf(" MENU DE OPCAES:\n\n ");
-   printf("------------------------");
-   printf("1 - Inserir produto: ");
-   printf("2 - Alterar produto: ");
-   printf("3 - Lista de produtos: ");
-   printf("4 - Vender produto: ");
-   printf("5 - Imprimir nota fiscal: ");
-   printf("6 - Sair do sistema: ");
-   printf("------------------------");
-   printf("\n\nESCOLHA UMA OPCAO : ");
+   printf(" \t MENU DE OPCAES:\n\n ");
+   printf("----------------------------------------\n");
+   printf(" \t 1 | Inserir produto:\n ");
+   printf(" \t 2 | Alterar produto:\n ");
+   printf(" \t 3 | Lista de produtos:\n ");
+   printf(" \t 4 | Vender produto:\n ");
+   printf(" \t 5 | Imprimir nota fiscal:\n ");
+   printf(" \t 6 | Sair do sistema:\n ");
+   printf("----------------------------------------");
+   printf("\n\t ESCOLHA UMA OPCAO : ");
    scanf("%d", &option);
    system("cls");
 
-   switch (option)
-   {
+   switch (option) {
    case 1:
       inserirProduto();
       break;
@@ -47,22 +49,42 @@ int main()
       imprimirNota();
       break;
    case 6:
-      sairSistema();
+      printf("FECHANDO PROGRAMA...");
       break;
-   return 0;}
+      return 0;
+   }
 }
 void inserirProduto()
-{   
+{
+   printf("\tPreencha algumas informações para cadastrar o  produto!\n");
+   printf("----------------------------------------");
+   printf("\nNome: ");
+   scanf("%s", &nome[cont]);
+
+   printf("\nQuantidade: ");
+   scanf("%f", &qntde[cont]);
+
+   printf("\nValor - R$: ");
+   scanf("%d", &valor[cont]);
+   cont++;
+
+   printf("Cadastro bem sucedido!\n\n");
+   system("pause");
 }
 void alterarProduto()
 {
+   printf("Qual a alteração?");
+
 }
 void listaProdutos()
-{   
+{
+   printf("lista de produtos");
 }
 void venderProduto()
 {
+   printf("Produtos para vender");
 }
 void imprimirNota()
 {
+   printf("Imprimir nota fiscal");
 }
