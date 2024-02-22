22722220a7ad62bd8c284672ae01d889823473d4
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-02-22 15:53:41 -0300

    Delete start.html/exemplo1.js

--- start.html/exemplo1.js
+++
@@ -1,9 +0,0 @@
-const frm= document.querySelector("form");
-const resp= document.querySelector("h3");
-
-frm.addEventListener("submit", (e) => {
-const nome= frm.in.Nome.value;
-resp.innerText = `Ola ${nome}`;
-e.preventDefault();
-
-});
