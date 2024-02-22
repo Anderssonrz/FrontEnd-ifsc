202e76310e4b9236e678452d29f919365b824c1b
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-02-22 17:26:37 -0300

    Update exemplo1.js

--- start.html/220224/exemplo1.js
+++ start.html/220224/exemplo1.js
@@ -2,8 +2,14 @@ const frm= document.querySelector("form");
 const resp= document.querySelector("h3");
 
 frm.addEventListener("submit", (e) => {
-const nome= frm.in.Nome.value;
-resp.innerText = `Ola ${nome}`;
-e.preventDefault();
+const nome= frm.inNome.value;
+const idade= frm.inIdade.value;
 
+resp.innerText = `Ola ${nome} sua idade é ${idade}`;
+e.preventDefault();
 });
+
+frm.addEventListener("reset", (e) => {
+    resp.innerText ="";
+    frm.inNome.focus();
+})
