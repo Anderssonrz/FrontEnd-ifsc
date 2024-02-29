14bfa9fe54621af6b42f530553941b2625779fa0
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-02-29 17:02:05 -0300

    Update ex3.js

--- start.html/220224/ex3.js
+++ start.html/220224/ex3.js
@@ -1,18 +1,11 @@
-document.getElementById('promoForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var descricao = document.getElementById('medicamento').value;
-    var preco = parseFloat(document.getElementById('preco').value);
+const frm= document.querySelector("form");
+
+frm.addEventListener("submit", (e) => {
+const descricao = document.getElementById('medicamento').value;
+const preco = parseFloat(document.getElementById('preco').value)
+const precoPromocional = Math.floor((preco * 2));
     
-    // Verifica se o preço é válido
-    if (isNaN(preco) || preco <= 0) {
-        alert('Por favor, insira um preço válido.');
-        return;
-    }
-    
-    // Calcula o valor da promoção
-    var precoPromocional = preco * 2 - Math.floor((preco * 2));
-    
-    // Exibe o resultado
-    document.getElementById('descricaoResultado').textContent = "Leve 2 por apenas R$ " + precoPromocional.toFixed(2);
-    document.getElementById('resultado').style.display = 'block';
+
+document.getElementById('descricaoResultado').textContent = "Leve 2 por apenas R$ " + precoPromocional.toFixed(2);
+e.preventDefault();
 });
