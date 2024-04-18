8c8428d776424ddf0f62740207f70d968ac2fb53
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-04-18 17:14:42 -0300

    Update ajax1ex.php

--- start.html/20240418/ajax1ex.php
+++ start.html/20240418/ajax1ex.php
@@ -1,24 +1,25 @@
 <pre><?php
 $filename = "clientes.csv";
-$file = fopen($filename, "r");
-if ($file) {
+$file = fopen($filename, "r");//tenta abrir arquivo
+if ($file) {//verifica se o arquivo foi aberto
     $arr = [];
-    while (!feof($file)) {
-        $line = fgets($file);
-        $line = str_replace(["\n", "\r"], '', $line);
-        if($line != ""){
+    while (!feof($file)) {// enquanto bnoa for final do arquivo
+        $line = fgets($file);//le uma linha do arquivo
+        $line = str_replace(["\n", "\r"], '', $line);//removo quebras de linha
+        if($line != ""){//se a linha nao esta vazia 
             print("*$line*\n");
 
         }
-        $record = explode(";",$line);
-        print_r($record);
-        array_push($arr, $record);
+        $record = explode(";",$line);//divide a string em pedacos
+        $record[0] = intval($record[0]);
+        print_r($record);//converte o codigo
+        array_push($arr, $record);//adiciona o registro no codigo
     }
     print_r($arr);
-    $json = json_encode($arr);
+    $json = json_encode($arr);//converte a array em json
     print_r("$json\n");
 }
-fclose($file);
+fclose($file);//fecha o arquivo
 
 // <?php
 // $filename ="ajax.txt";
