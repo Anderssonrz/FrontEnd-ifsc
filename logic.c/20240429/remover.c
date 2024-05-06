faad8084cb9528d357cfb8579fc917e6595edc78
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-05-06 18:52:49 -0300

    Update remover.c

--- logic.c/20240429/remover.c
+++ logic.c/20240429/remover.c
@@ -1,12 +1,9 @@
 /*faca um codigo que anule o primeiro vetor e mantenha o restante dos valores no vetor*/
-
-
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-
     int i = 0, quant=10 ;
     int vetor[] = {5, 8, 7, 1, 3, 0, 0, 0, 0, 0};
     int valor_incluir = 0;
@@ -19,9 +16,7 @@ int main()
     for (i = 0; i < quant - 1; i++){
     vetor[i] = vetor[i+1];
     }
-
-
-
+    
     printf ("Valores do vetor sem o primeiro numero: \n");
     for (i = 0; i < quant; i++)
     {
