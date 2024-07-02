7fdb99e0e09d7f5d3debbe7e6a954a158c68a3e0
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-07-02 17:13:20 -0300

    Update cadastro.php

--- start.html/20240702/cadastro.php
+++ start.html/20240702/cadastro.php
@@ -1,58 +1,74 @@
 <?php
-require ("conexao.php");
-if (isset($_POST["btnSalvar"])){
-      $caracteristica = '';
-      $caracteristica['nome'] = $_POST['txtNome'];
-      $caracteristica['descricao'] = $_POST['txtDescricao'];
-      cadastrarcaracteristica($conexao, $caracteristica);
+    require("conexao.php");
+
+    if (isset($_POST["btnSalvar"])) {
+        $caracteristica = [];
+        $caracteristica['nome'] = $_POST['txtNome'];
+        $caracteristica['descricao'] = $_POST['txtDescricao'];
+        cadastrarCaracteristica($conexao, $caracteristica);
     }
 ?>
 
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Cadastrar</title>
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous"
+    >
 
+    <style>
+        form {
+            margin: 1rem;
+        }
+
+        legend {
+            text-align: center;
+        }
+    </style>
 </head>
-<style>
-    form {
-        margin: 10px;
-    }
-</style>
 
 <body>
-    <form action="#" method="POST">
-        <div class="mb-3">
-            <label for="nomeproduto" class="form-label">Nome </label>
-            <input type="text" name="txtNome" class="form-control" id="nomeproduto">
-        </div>
-        <div class="mb-3">
-            <label for="descricaoproduto" class="form-label">Descrição</label>
-            <textarea type="text" name="txtDescricao" class="form-control" id="descricaoproduto"></textarea>
-        </div>
-        <button type="submit" name="btnSalvar" class="btn btn-primary">Cadastrar</button>
-    </form>
+    <fieldset>
+        <legend>Cadastrar Característica</legend>
+        <form action="#" method="POST">
+            <div class="mb-3">
+                <label for="nomeProduto" class="form-label">Nome</label>
+                <input type="text" name="txtNome" class="form-control" id="nomeProduto">
+            </div>
+            <div class="mb-3">
+                <label for="descricaoProduto" class="form-label">Descrição</label>
+                <textarea type="text" name="txtDescricao" class="form-control" id="descricaoProduto" cols="30" rows="5"></textarea>
+            </div>
+            <button type="submit" name="btnSalvar" class="btn btn-primary">Cadastrar</button>
+            <a class="btn btn-primary" href="index.php">Ver tabela</a>
+        </form>
+    </fieldset>
 </body>
 
 </html>
+
 <?php
-function cadastrarcaracteristica($conexao, $caracteristica)
-{
-    $nome = $caracteristica['nome'];
-    $descricao = $caracteristica['descricao'];
 
-    $sql= " INSERT INTO caracteristica(nome, descricao) VALUES ('$nome', '$descricao')";
+    function cadastrarCaracteristica($conexao, $caracteristica){
+        $nome = $caracteristica['nome'];
+        $descricao = $caracteristica['descricao'];
 
-    if(mysqli_query($conexao, $sql) == TRUE){
-        echo "Registro inserido com sucesso!";
-        header("Location : index.php");
-
-    }else{
-        echo "Erro ao inserir registro" . mysqli_connect_error();
+        $sql = "INSERT INTO caracteristica (nome, descricao) VALUES ('$nome', '$descricao')";
+        
+        if (mysqli_query($conexao, $sql) == TRUE){
+            echo "Registro inserido com sucesso!";
+            header("Location: index.php");
+        } else {
+            echo "Erro ao inserir registro " . mysqli_connect_error();
+        }
     }
-}
+
+?>
