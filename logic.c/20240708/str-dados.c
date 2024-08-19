63f86f6f4c433ea51a8c2c3bf4739500242126c4
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-08-19 15:22:49 -0300

    Update str-dados.c

--- logic.c/20240708/str-dados.c
+++ logic.c/20240708/str-dados.c
@@ -1,3 +1,5 @@
+//alteração de registro
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,12 +27,51 @@ int mostra_menu()
     printf("+------------------------+\n");
     printf("|     ler arquivo    | 5 |\n");
     printf("+------------------------+\n");
+    printf("|   ordenar arquivo  | 6 |\n");
+    printf("+------------------------+\n");
+    printf("|   editar registro | 7 |\n");
+    printf("+------------------------+\n");
     printf("|        Sair        | 0 |\n");
     printf("+------------------------+\n");
     printf("Tecle a opcao: ");
     scanf("%d", &opcao);
     return opcao;
 }
+int ordenar_produtos(tProd* vProd, int qtd){
+    float fTemp;
+    int iTemp, i;
+    char sTemp[50];
+
+    int houve_troca = 1;
+    while (houve_troca==1){
+        houve_troca = 0;
+        for (i=0; i<qtd-1; i++){
+            // se as descricoes est�o fora de ordem
+            if(strcmp(vProd[i].descricao,vProd[i+1].descricao)>0){
+                //efetua as trocas de valor
+
+                houve_troca = 1;
+
+                //primeiro troca o codigo
+                iTemp = vProd[i].codigo;
+                vProd[i].codigo = vProd[i+1].codigo;
+                vProd[i+1].codigo = iTemp;
+
+                // troca preco
+                iTemp = vProd[i].preco;
+                vProd[i].preco = vProd[i+1].preco;
+                vProd[i+1].preco = iTemp;
+
+                //troca descricao
+                strcpy(sTemp,vProd[i].descricao);
+                strcpy(vProd[i].descricao , vProd[i+1].descricao);
+                strcpy(vProd[i+1].descricao , sTemp);
+            }
+        }
+    }
+}
+
+
 int inclusao(tProd* vProd, int qtd)
 {
     system("cls");
@@ -51,59 +92,53 @@ int inclusao(tProd* vProd, int qtd)
     return qtd+1;
 }
 
-int listar(tProd* vProd, int qtd)
-{
-    system("cls");
-    int i = 0;
-    printf("--------------------------\n");
-    printf("Listagem Produtos\n");
-    printf("--------------------------\n");
 
-
-    for (i = 0; i < qtd; i++)
-    {
-        printf("| %5d | %-30s | %6.2f |\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
-    }
-    printf("--------------------------\n");
-    printf("| Total de Registros: %3d |\n", qtd);
-    printf("--------------------------\n");
-    system("pause");
-
+int listar(tProd* vProd, int qtd) {
+   int i;
+   system("cls");
+   printf("+-------------------------------------------------+\n");
+   printf("|              Listagem de Produtos               |\n");
+   printf("+-------------------------------------------------+\n");
+   for (i=0; i<qtd; i++) {
+      printf("| %1d | %-33s | %7.2f |\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
+   }
+   printf("+-------------------------------------------------+\n");
+   printf("| Total de Registros: %3d                         |\n", qtd);
+   printf("+-------------------------------------------------+\n");
+   system("pause");
 }
 
-int pesquisar(tProd* vProd, int qtd)
-{
-    system("cls");
-    int cont = 0;
-    int i = 0;
-    char buscar[50];
 
-    printf("--------------------------\n");
-    printf("Pesquisar Produtos\n");
-    printf("--------------------------\n");
+void print_spaces(int n) {
+   int i;
+   for (i=0; i<n; i++) {
+      printf(" ");
+   }
+}
 
-    fflush(stdin);
-    printf("Digite a descrisao para pesquisa: ");
-    gets(vProd[qtd].descricao);
+int pesquisar(tProd* vProd, int qtd) {
+   int i, cont=0;;
+   char buscar[50];
+   system("cls");
+   printf("Informe a Expressao de Busca: ");
+   fflush(stdin);
+   gets(buscar);
 
-    for (i = 0; i < qtd; i++)
-    {
-
-
-        if (strstr(strlwr(vProd[i].descricao), strlwr (buscar)) != 0)
-        {
-            printf("| %5d | %-30s | %6.2f |\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
-            printf("--------------------------\n");
-            cont ++;
-
-        }
-        printf("| Total de Registros Loca: %3d |\n", cont);
-        printf("--------------------------\n");
-        system("pause");
-    }
-
-
-
+   printf("+-------------------------------------------------+\n");
+   printf("| Pesquisando Produto '%s'",buscar);
+   print_spaces(26-strlen(buscar));
+   printf("|\n");
+   printf("+-------------------------------------------------+\n");
+   for (i=0; i<qtd; i++) {
+      if (strstr(strlwr(vProd[i].descricao), strlwr(buscar))!=NULL) {
+         printf("| %1d | %-33s | %7.2f |\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
+         cont++;
+      }
+   }
+   printf("+-------------------------------------------------+\n");
+   printf("| %3d Registros Encontrados                       |\n", cont);
+   printf("+-------------------------------------------------+\n");
+   system("pause");
 }
 
 int gravar_arquivo(tProd* vProd, int qtd)
@@ -121,7 +156,7 @@ int gravar_arquivo(tProd* vProd, int qtd)
     {
         for(i = 0; i < qtd; i++)
         {
-            fprintf(file, "%d;%s;%f\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
+            fprintf(file, "%d;%s;%f\r\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
         }
         printf("Arquivo gravado com sucesso!");
         system("pause");
@@ -152,19 +187,19 @@ int ler_arquivo(tProd* vProd )
             //printf("Numero de leituras: %d\n", cont);
             //precessar os campos da linha
             //codigo
-            token = strtok(linha, ";\n");
+            token = strtok(linha, ";\r\n");
             vProd[cont].codigo = atoi(token);
             //descricao
-            token= strtok(NULL, ";\n");
+            token= strtok(NULL, ";\r\n");
             strcpy(vProd[cont].descricao,token);
             //preco
-            token = strtok(linha, ";\n");
+            token = strtok(linha, ";\r\n");
             vProd[cont].preco = atof(token);
             cont++;
         }
+    printf("registos lidos\n");
     }
     fclose(file);
-    printf("registos lidos\n");
     system("pause");
     return cont;
 
@@ -195,6 +230,11 @@ int main()
             break;
         case 5:
             ler_arquivo(vProd);
+            break;
+        case 6:
+            ordenar_produtos(vProd, qtd);
+            break;
+
             break;
         case 0:
             printf("Encerrando...");
